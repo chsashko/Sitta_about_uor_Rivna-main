f3e8944e2b73b21bc6632c1569e3767bee85329e
DanilGhosty <vip.korotkov25@gmail.com>
2022-04-26 17:15:06 +0300

    tourist_rivne-animations

--- tourist_Rivne/script.js
+++ tourist_Rivne/script.js
@@ -1,11 +1,34 @@
 const fort_anim = document.querySelector(".fort");
-const fort_info_anim = document.querySelector(".fort-info")
-const fort_title_anim = document.querySelector(".fort-title")
+const fort_info_anim = document.querySelector(".fort-info");
+const fort_title_anim = document.querySelector(".fort-title");
 
+const zal_anim = document.querySelector(".zal");
+const zal_info_anim = document.querySelector(".zal-info");
+const zal_title_anim = document.querySelector(".zal-title");
+
+const bunker_anim = document.querySelector(".bunker");
+const bunker_info_anim = document.querySelector(".bunker-info");
+const bunker_title_anim = document.querySelector(".bunker-title");
+
+const height = fort_anim.height;
+fort_anim.classList.add("-active");
+fort_info_anim.classList.add("-active")
+fort_title_anim.classList.add("-active");
 function anim() {
-	fort_anim.classList.add("-active")
-	fort_info_anim.classList.add("-active")
-	fort_title_anim.classList.add("-active")
-}
+	console.log(window);
 
+}
+window.addEventListener("scroll", function(){
+	console.log(bunker_anim);
+	if(window.scrollY >= 350){
+		zal_anim.classList.add("-active");
+		zal_info_anim.classList.add("-active")
+		zal_title_anim.classList.add("-active");
+	}
+	if(window.scrollY >= 1000){
+		bunker_anim.classList.add("-active");
+		bunker_info_anim.classList.add("-active")
+		bunker_title_anim.classList.add("-active");
+	}
+})
 setTimeout(anim, 1)
