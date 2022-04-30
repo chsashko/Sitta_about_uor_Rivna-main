c0c572d7015f79fd62b4271afa64911297d2eee5
chsashko <70292721+chsashko@users.noreply.github.com>
2022-04-30 21:50:11 +0300

    fix someone problems
    create some problems

--- history_Rivne/burger_script.js
+++ history_Rivne/burger_script.js
@@ -1,22 +1,21 @@
-let menu  = document.querySelector('.burger-menu');
+let menu = document.querySelector('.burger-menu');
 
 let links = document.querySelector('.burger-menu_link');
 let overlay = document.querySelector('burger-menu_overlay');
 let button = document.querySelector('.burger-menu_button');
-button.addEventListener('click',(e)=>{
+button.addEventListener('click', (e) => {
     e.preventDefault();
     toggleMenu();
-} )
+})
 
-function toggleMenu(){
+function toggleMenu() {
     menu.classList.toggle('burger-menu_active');
-    if (menu.classList.contains('burger-menu_active')){
-        document.querySelector('body').css('overflow', 'hidden')
-    }
-    else{
-        document.querySelector('body').css('overflow', 'visible')
+    if (menu.classList.contains('burger-menu_active')) {
+        document.querySelector('body').css('overflow', 'hidden');
+    } else {
+        document.querySelector('body').css('overflow', 'visible');
     }
 }
 
-links.addEventListener('click',()=>toggleMenu());
-overlay.addEventListener('click',()=>toggleMenu());
+// links.addEventListener('click', () => toggleMenu());
+// overlay.addEventListener('click',()=>toggleMenu());
