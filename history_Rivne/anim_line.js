b224c54e0f343d286d97921ea12acedb1059c9b5
vadymvv <vadymolhovyk@gmail.com>
2022-05-04 18:31:59 +0300

    history carousel and scroll

--- history_Rivne/anim_line.js
+++ history_Rivne/anim_line.js
@@ -1,4 +1,7 @@
 const circle = document.querySelector(".circle_years");
 window.addEventListener('scroll', function(){
-    console.log(circle.offsetTop + Number(window.srollY));
+    if(window.scrollY){
+        circle.style.transform = `translateY(` + window.scrollY + `px` + `)`;
+    }
+    
 })
