83b2a4c24dd161a8f18cb2d77d62c2b61e465e16
Kikono1014 <vitalik.pyvovarchuk@gmail.com>
2022-05-11 18:33:12 +0300

    small changes in history

--- history_Rivne/anim_line.js
+++ history_Rivne/anim_line.js
@@ -1,7 +1,10 @@
 const circle = document.querySelector(".circle_years");
+const year = document.querySelector(".year");
 window.addEventListener('scroll', function(){
     if(window.scrollY){
         circle.style.transform = `translateY(` + window.scrollY + `px` + `)`;
     }
-    
+    if(this.window.scrollY == 100){
+
+    } 
 })
