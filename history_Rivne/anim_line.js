3c043eff591cf1b8d1f391aa2b583f4842f1e117
vadymvv <vadymolhovyk@gmail.com>
2022-05-17 18:44:54 +0300

    text_changes_year

--- history_Rivne/anim_line.js
+++ history_Rivne/anim_line.js
@@ -3,8 +3,12 @@ const year = document.querySelector(".year");
 window.addEventListener('scroll', function(){
     if(window.scrollY){
         circle.style.transform = `translateY(` + window.scrollY + `px` + `)`;
-    }
-    if(this.window.scrollY == 100){
 
-    } 
+    }
+    if(this.window.scrollY <= 420){
+        year.innerHTML = "1914-1918"; 
+    }
+    if(this.window.scrollY >= 420){
+        year.innerHTML = "1939-1945";     
+    }
 })
